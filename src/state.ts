39e169b3dd019987f8fc377a05df199f0b361d1c
alvin.wang <alvin.wang@binance.com>
2022-05-12 23:05:09 +0200

    fix: commit state multi interval

--- src/state.ts
+++ src/state.ts
@@ -1,9 +1,18 @@
-export function initState() {
-  let count = 0;
-  setInterval(() => {
-    let countEle = document.getElementById('count');
-    countEle!.innerText =  ++count + '';
-
-  }, 1000);
+// state.ts
+let timer: number | undefined;
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => {
+    if (timer) {
+      clearInterval(timer);
+    }
+  })
+	
 }
 
+export function initState() {
+  let count = 0;
+  timer = setInterval(() => {
+    let countEle = document.getElementById('count');
+    countEle!.innerText =  ++count + '';
+  }, 1000);
+}

332f3e26f88a8a677f33b4eed04e59c731a1065e
alvin.wang <alvin.wang@binance.com>
2022-05-12 23:09:39 +0200

    fix: add global count

--- src/state.ts
+++ src/state.ts
@@ -1,18 +1,31 @@
 // state.ts
 let timer: number | undefined;
 if (import.meta.hot) {
+	 // 初始化 count
+	if (!import.meta.hot.data.count) {
+		import.meta.hot.data.count = 0;
+	}
+
   import.meta.hot.dispose(() => {
     if (timer) {
       clearInterval(timer);
     }
   })
-	
+
 }
 
 export function initState() {
   let count = 0;
+	const getAndIncCount = () => {
+		const data = import.meta.hot?.data || {
+			count: 0
+		};
+		data.count = data.count + 1;
+		return data.count;
+	};
+
   timer = setInterval(() => {
     let countEle = document.getElementById('count');
-    countEle!.innerText =  ++count + '';
+		countEle!.innerText =  getAndIncCount() + '';
   }, 1000);
 }

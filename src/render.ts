6729aea06c482626a254c7a0a00628971fbaf238
alvin.wang <alvin.wang@binance.com>
2022-05-12 22:38:44 +0200

    chore: add hot accept

--- src/render.ts
+++ src/render.ts
@@ -4,6 +4,11 @@ export const render = () => {
   const app = document.querySelector<HTMLDivElement>('#app')!
   app.innerHTML = `
     <h1>Hello Vite!</h1>
-    <p target="_blank">This is hmr test.123</p>
+    <p target="_blank">This is hmr test.1234</p>
   `
+}
+
+// 条件守卫
+if (import.meta.hot) {
+	import.meta.hot.accept((mod) => mod.render())
 }
